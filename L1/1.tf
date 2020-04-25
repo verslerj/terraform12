af54aa7692aa2b560a1ab070fd025c56f3a51521
Janos Versler <me@example.com>
2020-04-26 01:24:45 +0200

    Lesson1

--- L1/1.tf
+++ L1/1.tf
@@ -5,9 +5,19 @@ provider "aws" {
 resource "aws_instance" "my_ubuntu" {
     ami = "ami-068663a3c619dd892"
     instance_type = "t2.micro"
+    tags = {
+      Name = "UbuntuSrv"
+      Owner = "Janos Versler"
+      Project = "terraform12"
+    }
 }
 
 resource "aws_instance" "amazon_linux" {
     ami = "ami-0323c3dd2da7fb37d"
     instance_type = "t2.micro"
+    tags = {
+      Name = "AmazonLinuxSrv"
+      Owner = "Janos Versler"
+      Project = "terraform12"
+    }
 }

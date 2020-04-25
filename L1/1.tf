8ea60696d67926360488b9970c8f1638fe9c9f41
Janos Versler <me@example.com>
2020-04-26 00:59:27 +0200

    Lesson1

--- L1/1.tf
+++ L1/1.tf
@@ -6,3 +6,8 @@ resource "aws_instance" "my_ubuntu" {
     ami = "ami-068663a3c619dd892"
     instance_type = "t2.micro"
 }
+
+resource "aws_instance" "amazon_linux" {
+    ami = "ami-0323c3dd2da7fb37d"
+    instance_type = "t2.micro"
+}

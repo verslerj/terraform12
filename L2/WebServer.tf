8bb5d4aa4db2fd7aee34f5b6959491a583c7f19c
Janos Versler <me@example.com>
2020-04-26 16:16:27 +0200

    L2 added with webserver

---
+++ L2/WebServer.tf
@@ -0,0 +1,58 @@
+#---------------------------------------------
+# My Terraform
+#
+# Build WebServer during bootstrap
+#
+# Made by Janos Versler
+#---------------------------------------------
+
+
+provider "aws" {
+    region        = "eu-east-1"
+  }
+
+resource "aws_instance" "my_webserver" {
+    ami           = "ami-0323c3dd2da7fb37d"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = ["aws_security_group.my_webserver.id"]
+    user_data = <<EOF
+#!/bin/bash
+yum -y update
+yum -y install httpd
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform12!"  >  /var/www/html/index.html
+sudo service httpd start
+chkconfig httpd on
+EOF
+}
+
+resource "aws_security_group" "my_webserver" {
+  name = "WebServer Security Group"
+  description = "My First SecurityGroup"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web Server SecurityGroup"
+    Owner = "Janos Versler"
+  }
+}

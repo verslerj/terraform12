7fbee60d1e0f5ba6454bc5eb3aa53b8e82693672
Janos Versler <me@example.com>
2020-04-26 23:14:55 +0200

    L2 added with webserver

--- L3/WebServer.tf
+++ L3/WebServer.tf
@@ -15,7 +15,16 @@ resource "aws_instance" "my_webserver" {
     ami                    = "ami-0323c3dd2da7fb37d"
     instance_type          = "t2.micro"
     vpc_security_group_ids = [aws_security_group.my_webserver.id]
-    user_data              = file("user_data.sh")
+    user_data              = <<EOF
+#!/bin/bash
+yum -y update
+yum -y install httpd
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform12!"  >  /var/www/html/index.html
+sudo service httpd start
+chkconfig httpd on
+EOF
+
 tags = {
   Name   = "Web Server built by terraform12"
   Owner  = "Janos Versler"
